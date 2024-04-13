2d89b0d742f417a50ec124ab639ca0191716b12d
Cris02Vanegas <115751466+Cris02Vanegas@users.noreply.github.com>
2024-04-12 22:51:17 -0500

    Se agregan fotos y JS

---
+++ JS/contacto.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* const $nombre = document.getElementById('nombre');
+const $correo = document.getElementById('correo');
+const $telefono = document.getElementById('telefono');
+const $mensaje = document.getElementById('mensaje');
+const $botonEnviar = document.getElementById('enviarMensaje');
+
+let nombre = null;
+let correo = null;
+let telefono = null;
+let mensaje = null;
+
+$nombre.addEventListener('input', (evento) => {
+    nombre = evento.target.value;
+});
+
+$correo.addEventListener('input', (evento) => {
+    email = evento.target.value;
+});
+
+$mensaje.addEventListener('input', (evento) => {
+    mensaje = evento.target.value;
+});
+
+$botonEnviar.addEventListener('click', (evento) => {
+    evento.preventDefault();
+    alert(nombre);
+    alert(correo);
+    alert(telefono);
+    alert(mensaje);
+}); */
